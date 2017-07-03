dde603ad86f00137b949087c2420c23642bcd5b6
Melody Ann Seda Marotte <melshyrule@fb.com>
2017-07-03 13:56:01 -0700

    added consumer key, test run

--- twitter_alamofire_demo/APIManager.swift
+++ twitter_alamofire_demo/APIManager.swift
@@ -15,8 +15,8 @@ import KeychainAccess
 class APIManager: SessionManager {
     
     // MARK: TODO: Add App Keys
-    static let consumerKey = "YOUR_KEY_HERE"
-    static let consumerSecret = "YOUR_SECRET_HERE"
+    static let consumerKey = "ejIJuMpPUgyJyszKxY9B0oYNy"
+    static let consumerSecret = "RKS71Yj3QdvrdgwlzhKvllgn2iCltmltA0HjktcY9Dzerzmvl1"
     
     static let requestTokenURL = "https://api.twitter.com/oauth/request_token"
     static let authorizeURL = "https://api.twitter.com/oauth/authorize"
