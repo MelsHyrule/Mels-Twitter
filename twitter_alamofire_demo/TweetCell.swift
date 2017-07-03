fa9cdc90c566546dd9269d28402713790e5d6923
Melody Ann Seda Marotte <melshyrule@fb.com>
2017-07-03 15:02:10 -0700

    Adding elements to tweet cell

--- twitter_alamofire_demo/TweetCell.swift
+++ twitter_alamofire_demo/TweetCell.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class TweetCell: UITableViewCell {
     
-    @IBOutlet weak var tweetTextLabel: UILabel!
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var screenNameLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var datePostedLabel: UILabel!
+    @IBOutlet weak var tweetTextView: UITextView!
+    @IBOutlet weak var replyCountLabel: UILabel!
+    @IBOutlet weak var retweetCountLabel: UILabel!
+    @IBOutlet weak var likeCountLabel: UILabel!
     
     var tweet: Tweet! {
         didSet {
-            tweetTextLabel.text = tweet.text
+            
+            //userImageView.image =
+            screenNameLabel.text = tweet.user.screenName!
+            nameLabel.text = tweet.user.name as! String
+            datePostedLabel.text = tweet.createdAtString
+            tweetTextView.text = tweet.text
+            //replyCountLabel.text =
+            retweetCountLabel.text = tweet.retweetCount as! String
+            likeCountLabel.text = tweet.favoriteCount as! String
+            
         }
     }
     
@@ -22,6 +38,45 @@ class TweetCell: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
     }
+        
+    
+    @IBAction func replyButtonPressed(_ sender: Any) {
+        
+        
+        
+    }
+    
+    
+    
+    @IBAction func retweetButtonPressed(_ sender: Any) {
+        if (tweet.retweeted) {
+            tweet.retweeted = false
+            tweet.retweetCount = tweet.retweetCount - 1
+            retweetCountLabel.text = tweet.retweetCount as! String
+        } else {
+            tweet.retweeted = true
+            tweet.retweetCount = tweet.retweetCount + 1
+            retweetCountLabel.text = tweet.retweetCount as! String
+        }
+    }
+    
+    
+    @IBAction func likeButtonPressed(_ sender: Any) {
+        if (tweet.favorited!) {
+            tweet.favorited = false
+            tweet.favoriteCount = tweet.favoriteCount! - 1
+            likeCountLabel.text = tweet.favoriteCount as! String
+        } else {
+            tweet.favorited = true
+            tweet.favoriteCount = tweet.favoriteCount! + 1
+            likeCountLabel.text = tweet.favoriteCount as! String
+
+        }
+    }
+    
+    
+    
+    
     
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
